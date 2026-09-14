710b2a55e728f57e40ebed68f3cde274d4279fe4
rsm-msaad <msaad@ucsd.edu>
2026-09-13 20:25:44 -0700

    Add AdvisorCalendarConnection model and graph_event notification kind

--- backend/rsm_thrive/models/__init__.py
+++ backend/rsm_thrive/models/__init__.py
@@ -2,7 +2,7 @@ from .academic import (  # noqa: F401
     Assignment, Course, CourseMeeting, Enrollment, StudentAssignment, Syllabus,
 )
 from .appointments import (  # noqa: F401
-    Advisor, Appointment, AppointmentNotification, AppointmentSlot,
+    Advisor, AdvisorCalendarConnection, Appointment, AppointmentNotification, AppointmentSlot,
 )
 from .chat import (  # noqa: F401
     ChatMessage, ChatTurnLog, Conversation, TurnFeedback,
@@ -17,10 +17,10 @@ from .overlay import (  # noqa: F401
     CalendarPrefs, EventJoin, IgnoredEvent, SharedTask, StudentTask, TaskNote,
     TaskOverride,
 )
-from .planner import CoursePlan, PlannerSession  # noqa: F401
 from .personal import (  # noqa: F401
     CalendarItemLabel, CalendarItemUrgent, CustomCalendarEvent, QuickListItem,
 )
+from .planner import CoursePlan, PlannerSession  # noqa: F401
 from .requests import CourseRequest  # noqa: F401
 from .resources import ResourceLink  # noqa: F401
 from .resume import (  # noqa: F401
